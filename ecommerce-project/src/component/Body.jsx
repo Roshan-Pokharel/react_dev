6c4a0a39e3b31af009fd06d2249435f8a310df62
Roshan Pokharel <roshanpokharel890@gmail.com>
2025-11-16 13:06:01 +0545

    adding quantity for product

--- ecommerce-project/src/component/Body.jsx
+++ ecommerce-project/src/component/Body.jsx
@@ -1,7 +1,5 @@
-import PriceCents from '../utils/priceCents'
 import {Header} from '../component/Shared/Header.jsx'
-import axios from 'axios';  
-import {useState} from 'react';
+import {Product} from './home/product.jsx'
 import './Shared/General.css';
 import './Shared/Header.css';
 import './Home.css';
@@ -16,7 +14,7 @@ import './Home.css';
 
  function Body({products, loadCart}){
 
- const [quantity , setQuantity] = useState(1);
+
   
 
    //const [products, setProducts] = useState([]);
@@ -48,67 +46,7 @@ import './Home.css';
         {products.map((product) => {
          
          return(
-          <div key={product.id} className="product-container">
-          <div className="product-image-container">
-            <img className="product-image"
-              src={product.image} />
-          </div>
-
-          <div className="product-name limit-text-to-2-lines">
-            {product.name}
-          </div>
-
-          <div className="product-rating-container">
-            <img className="product-rating-stars"
-              src={`images/ratings/rating-${(product.rating.stars)*10}.png`}/>
-            <div className="product-rating-count link-primary">
-              {product.rating.count}
-            </div>
-          </div>
-
-          <div className="product-price">
-           {PriceCents(product.priceCents)}
-          </div>
-
-          <div className="product-quantity-container"
-          >
-            <select value={quantity} onChange={(event)=>{
-              const quantitySelected = Number(event.target.value);
-              setQuantity(quantitySelected)
-              console.log(quantitySelected);
-            }}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div className="product-spacer"></div>
-
-          <div className="added-to-cart">
-            <img src="images/icons/checkmark.png" />
-            Added
-          </div>
-
-          <button className="add-to-cart-button button-primary" 
-          onClick={async()=>{
-           await axios.post('http://localhost:3000/api/cart-items', {
-              productId: product.id,
-              quantity:1
-            });
-            await loadCart();
-          }}>
-            Add to Cart
-          </button>
-        </div>
+         <Product key={product.id} product ={product} loadCart={loadCart} />
  )})}
         
     
